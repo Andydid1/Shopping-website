f2b73f1a76a01819e44dc794f5e3841c6c251aa7
Andydid1 <47250660+Andydid1@users.noreply.github.com>
2020-12-07 23:38:26 +0800

    Initial commit
    The website is capable of login, signup, purchase, deposit, change address, create products and inventories and so on.

---
+++ add_product.php
@@ -0,0 +1,39 @@
+<?php
+    session_start();
+    if(!empty($_SESSION['err'])){
+        echo "Error";
+        $_SESSION['err'] = null;
+    }
+    if(empty($_SESSION['login'])){
+        header("Location: inventory.php");
+    }
+    $curr_i_id = $_GET['i_id'];
+?>
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>Add product</h1>
+        Note: product name must be unique
+        <form action='process_add_p.php?i_id=<?php echo $curr_i_id?>' method="post">
+            <div>
+                Product name: <input type='text' name='p_name'>
+            </div>
+            <div>
+                Description: <input type='text' name='description'>
+            </div>
+            <div>
+                Unit price <input type='text' name='unit_price'>
+            </div>
+            <div>
+                Unit <input type='text' name='unit'>
+            </div>
+            <div>
+                Amount <input type='text' name='amount'>
+            </div>
+            <div>
+                <input type='submit' value='Submit'>
+            </div>
+        </form>
+        <a href="inventory.php?i_id=<?php echo $curr_i_id;?>">Back to inventory</a>
+    </body>
+</html>
