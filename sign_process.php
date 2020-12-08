b5f2ea3eafea8475a42c0670bfbb27b3121c9264
Andy <wanggd99@hotmail.com>
2020-12-08 22:06:12 +0800

    Initial Commit

--- sign_process.php
+++ sign_process.php
@@ -5,7 +5,9 @@
     if($u_pw1 != '' && $u_pw2 != '' && $u_pw1 == $u_pw2){
         $u_nm = $_POST['u_nm'];
         $u_pw = sha1($u_pw1);
-        $dbc = mysqli_connect('localhost','Andy','1116','test_db');
+        include_once "mysql_info.php";
+        $dbc = mysqli_connect(constant("DB_H"),
+        constant("DB_U"),constant("DB_P"),constant("DB_DB"));
         $query = "INSERT INTO user_info (u_name, u_pw, balance) VALUES ('{$u_nm}','{$u_pw}',1000);";
         mysqli_query($dbc,$query);
         $nquery = "SELECT * from user_info where u_name = '{$u_nm}';";
